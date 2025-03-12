1f7548f4e06a829e124c990ffc8704ead2b04024
Spiros Evangelatos <sevangelatos@gmail.com>
2025-03-12 23:01:51 +0200

    Add single audio file functionality

--- vid_from_audio.bat
+++ vid_from_audio.bat
@@ -1,15 +1,20 @@
 
 REM Downscaling input image first allows us to encode faster 
-tools\ffmpeg.exe -y -i %1\image.jpg -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2"  %1\scaled.png
+tools\ffmpeg.exe -y -i "%~1\image.jpg" -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2" -update true -frames:v 1 "%~1\scaled.png"
 
-REM Create a playlist of intro + 48 loops
-echo file '%~1\intro.mp3' > "%~1\playlist.txt"
-for /l %%x in (1, 1, 48) do (
-   echo file '%~1\loop.mp3' >> "%~1\playlist.txt"
+if exist "%~1\single.mp3" (
+	REM Audio is coming from a single file
+	echo file '%~1\single.mp3' > "%~1\playlist.txt"
+) else (
+	REM Create a playlist of intro + 48 loops
+	echo file '%~1\intro.mp3' > "%~1\playlist.txt"
+	for /l %%x in (1, 1, 48) do (
+	   echo file '%~1\loop.mp3' >> "%~1\playlist.txt"
+	)
 )
 
 REM use ffmpeg to create one big wav
-tools\ffmpeg.exe -f concat -safe 0 -i %1\playlist.txt -c copy "%~1\audio.wav"
+tools\ffmpeg.exe -f concat -safe 0 -i "%~1\playlist.txt" -c copy "%~1\audio.wav"
 
 REM Now use ffmpeg to render final video. 
 REM Raising CRF from 25 up to 50 will give smaller files
